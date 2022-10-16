f2bb9e0e0ea59559f16dc0a4bccc52913a74b95d
Wuffluv <90496817+Wuffluv@users.noreply.github.com>
2022-10-17 00:23:00 +0900

    Update Test Git.cpp

--- Git/Test Git/Test Git/Test Git.cpp
+++ Git/Test Git/Test Git/Test Git.cpp
@@ -6,4 +6,5 @@ int main()
 {
     cout << "Hello Git!" << endl;
     cout << "Git is free and open source software for distributed version control" << endl;
+    cout << "GitHub changed my code((" << endl;
 }
