8c692f22e7649619749cacae49e70b7f8241a7f4
Wuffluv <rodion.rycha@yandex.ru>
2022-10-26 12:06:33 +0900

    created class

---
+++ class/Auto Engine/Class of Auto Engine/Class of Auto Engine/UnitClass.cpp
@@ -0,0 +1,14 @@
+#include "Class.h"
+
+using namespace std;
+
+Engine :: Engine() {
+	EngineCapacity = ""; //Объем двигателя
+	PowerSupplySystem = ""; //Система питания
+	Fuel = ""; //Тип топлива
+	Resource = 0; //Максимальный пробег
+	TypeOil = ""; //Тип масла
+	PowerDVS = ""; //Мощность ДВС
+	Expenditure = 0; //Расход топлива
+	ModelEngine = ""; //Модель двигателя
+}
